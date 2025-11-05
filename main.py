754b23f9a6a0161c0e2ae29db7963938d7d56d2c
nirrahav <nirrahav1996@gmail.com>
2025-11-05 19:40:57 +0200

    Git Practice

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import requests
+
+def get_random_joke():
+    response = requests.get("https://official-joke-api.appspot.com/random_joke")
+    if response.status_code == 200:
+        data = response.json()
+        print(f"{data['setup']}")
+        print(f"👉 {data['punchline']}")
+    else:
+        print("Couldn't fetch a joke right now 😅")
+
+if __name__ == "__main__":
+    print("Fetching a random joke for you...\n")
+    get_random_joke()
