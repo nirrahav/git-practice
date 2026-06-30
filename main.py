f51c14f3221b2005b2840aacb6df6613b9290f7f
nirrahav <nirrahav1996@gmail.com>
2026-06-30 13:51:23 +0300

    try

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def get_random_joke():
         print(f"{data['punchline']}")
     else:
         print("Couldn't fetch a joke right now 😅")
-
+def greeting():
+    print("Welcome to the joke!")
 if __name__ == "__main__":
     print("Fetching a random joke for you...\n")
     get_random_joke()
